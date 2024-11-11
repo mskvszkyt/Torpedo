9c0bd5c981cbcfa1b556d0962c80879ff21bd317
muskovszky.tamas <muskovszky.tamas@BURG-1E06-03>
2024-11-11 13:50:21 +0100

    initial commit

---
+++ Torpedo-server/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Torpedo_server
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
